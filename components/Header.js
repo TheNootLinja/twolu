5d7d7ac557269b1d7a3feafb8757873e76daf712
TheNootLinja <thenootlinja@gmail.com>
2022-03-13 22:35:45 -0400

    Add HeaderItem component to Header with appropriate props

--- components/Header.js
+++ components/Header.js
@@ -1,12 +1,25 @@
 import Image from 'next/image'
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningBoltIcon,
+  SearchIcon,
+  UserIcon
+  } from "@heroicons/react/outline"
 
 import HeaderItem from './HeaderItem';
 
 function Header() {
   return (
-    <header className=''>
-      <div>
-        <HeaderItem title='home' Icon=''/>
+    <header className='mt-5'>
+      <div className='flex flex-grow justify-evenly max-w-2xl'>
+        <HeaderItem title='Home' Icon={HomeIcon}/>
+        <HeaderItem title='Trending' Icon={LightningBoltIcon}/>
+        <HeaderItem title='Verified' Icon={BadgeCheckIcon}/>
+        <HeaderItem title='Collections' Icon={CollectionIcon}/>
+        <HeaderItem title='Search' Icon={SearchIcon}/>
+        <HeaderItem title='Account' Icon={UserIcon}/>
       </div>
       <Image 
         className='object-contain'
